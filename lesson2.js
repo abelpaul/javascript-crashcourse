7550e55bc44a733dcad8b9df7c0f27eed30f5f15
Abel George <abel.paul.george@gmail.com>
2018-08-22 13:45:40 -0400

    lesson 2 edit

---
+++ lesson2.js
@@ -0,0 +1,15 @@
+// entering a number returns it right back //
+3
+// comparisons return boolean
+3 > 10 
+//comments don't need the ending slashes, my b
+/* this comment is multi line
+woahhhhh
+esketti 
+*/
+//string concat
+"Hello " + "Big Boi";
+
+// vars
+var greeting = "Yo";
+greeting + " Homie";
