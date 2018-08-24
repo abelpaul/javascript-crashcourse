46fa57239bed9f6dffc965e5230ceae6e1e5565e
Abel George <abel.paul.george@gmail.com>
2018-08-23 20:27:36 -0400

    8/23 lessons

---
+++ ifstatements.js
@@ -0,0 +1,12 @@
+// if statements
+
+let example = 3;
+
+if(example==5 && true === true) {
+    console.log("true");
+} else if( false || true ) {
+    console.log("hi")
+}
+else {
+    console.log("hello")
+}
