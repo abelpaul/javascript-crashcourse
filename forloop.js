652612e8e4aefb2dc62a705c6fe782e3e831e909
Abel George <abel.paul.george@gmail.com>
2018-08-23 20:36:27 -0400

    added lessons 8/23

---
+++ forloop.js
@@ -0,0 +1,17 @@
+// for loops
+
+let total = 0
+
+
+let numArray = [10,12,12,12,24,545]
+
+
+for (let i = 0; i < 5; i++) {
+    total++;
+    console.log(total);
+}
+
+for( let i = 0; i < numArray.length; i++) {
+    numArray[i]++;
+    console.log(numArray);
+}
---
+++ switch.js
@@ -0,0 +1,13 @@
+// switch statement
+
+let studentAnswer = 'A';
+
+switch(studentAnswer){
+    case 'A':
+    console.log("hi");
+    break;
+    case 'B':
+    console.log("hello");
+    break;
+    
+}
