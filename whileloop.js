9664f6c55d5fbdbc9e778251d98a311b2c8eb4e6
Abel George <abel.paul.george@gmail.com>
2018-08-23 20:39:32 -0400

    added lesson 8/23

---
+++ whileloop.js
@@ -0,0 +1,18 @@
+// while
+
+let count = 0;
+
+while(count < 20) {
+    count++;
+}
+
+console.log(count);
+
+
+// dowhile will run at least once
+
+do {
+    count++;
+}
+while(false);
+console.log(count);
