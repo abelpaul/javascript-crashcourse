ee08d94431145bbbafa2d65ac4cbb073ebc28af0
Abel George <abel.paul.george@gmail.com>
2018-08-26 17:49:32 -0400

    added JS Stalker

---
+++ JS Stalker/index.js
@@ -0,0 +1,5 @@
+let firstname = prompt("What is Your First Name");
+let lastname = prompt("What is Your Last Name");
+let age = prompt("What is age");
+console.log(firstname + " " + lastname);
+console.log(age);
